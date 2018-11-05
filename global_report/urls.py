166fae7b489756882fbef7b8b42feaa8517170c9
gameurator <41478867+gameurator@users.noreply.github.com>
2018-11-05 22:58:29 +0100

    global_report app added

--- global_report/urls.py
+++ global_report/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('global_report/', include('blog.urls')),
+    path('global_report/', include('global_report.urls')),
 ]

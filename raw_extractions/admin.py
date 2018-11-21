38630c97a2ea5089913f62f06ebb9f2bb497d0a5
Arnaud de Coudenhove <arnaud.decoudenhove8@gmail.com>
2018-11-21 17:36:21 +0100

    DB models created

--- raw_extractions/admin.py
+++ raw_extractions/admin.py
@@ -1,16 +1,38 @@
 from django.contrib import admin
-from .models import Device, ControllerType
+from .models import Device, ControllerType, EnergyName, SubDevice
 
 
 # Register your models here.
 
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('device_id', 'name', 'energy_names')
-    list_filter = ('name', 'energy_names',)
-    # date_hierarchy = 'date'
+    list_display = ('device_name',)
+    list_filter = ('device_name',)
+    ordering = ('device_name',)
+    search_fields = ('device_name',)
+
+
+class SubDeviceAdmin(admin.ModelAdmin):
+    list_display = ('device_name', 'category')
+    list_filter = ('device_name', 'category')
+    ordering = ('category', 'device_name')
+    search_fields = ('device_name', 'category')
+
+
+class ControllerTypeAdmin(admin.ModelAdmin):
+    list_display = ('category',)
+    list_filter = ('category',)
+    ordering = ('category',)
+    search_fields = ('category',)
+
+
+class EnergyNameAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
     ordering = ('name',)
-    search_fields = ('name', 'energy_names')
+    search_fields = ('name',)
 
 
-admin.site.register(ControllerType)
-admin.site.register(Device)
+admin.site.register(ControllerType, ControllerTypeAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(EnergyName, EnergyNameAdmin)
+admin.site.register(SubDevice, SubDeviceAdmin)
--- raw_extractions/models.py
+++ raw_extractions/models.py
@@ -1,18 +1,37 @@
 from django.db import models
 
 
-# Create your models here.
-class Device(models.Model):
-    device_id = models.IntegerField(default=0, unique=True, verbose_name="device_id")
-    device_name = models.CharField(max_length=12, unique=True, verbose_name="device_name")
-    energy_names = models.ForeignKey('EnergyNames', verbose_name='Energy Names')
-    category = models.ManyToManyField('ControllerType', verbose_name="Category")
-
-
 class ControllerType(models.Model):
     category = models.CharField(max_length=50, unique=True, verbose_name="Category")
 
+    def __str__(self):
+        return self.category
 
-class EnergyNames(models.Model):
-    name = models.CharField(unique=True, verbose_name="Energy Name")
-    category = models.ManyToManyField('ControllerType', verbose_name="Category")
+
+class EnergyName(models.Model):
+    name = models.CharField(verbose_name="Energy Name", unique=True, max_length=50)
+    category = models.ForeignKey('ControllerType', verbose_name="Category", on_delete='PROTECT')
+
+    def __str__(self):
+        return self.name
+
+
+class SubDevice(models.Model):
+    device_id = models.IntegerField(default=0, unique=True, verbose_name='Device Id')
+    device_name = models.ForeignKey('Device', verbose_name='Device Name', on_delete='PROTECT')
+    energy_names = models.ManyToManyField('EnergyName', verbose_name='Energy Name')
+    category = models.ForeignKey('ControllerType', on_delete='PROTECT', verbose_name='Category')
+
+    def __str__(self):
+        return self.device_name + self.category
+
+
+class Device(models.Model):
+    # device_id = models.IntegerField(default=0, unique=True, verbose_name="device_id")
+    device_name = models.CharField(max_length=12, unique=True, verbose_name="device_name")
+
+    # energy_names = models.ManyToManyField('EnergyName', verbose_name='Energy Name')
+    # category = models.ForeignKey('ControllerType', verbose_name="Category", on_delete='PROTECT')
+
+    def __str__(self):
+        return self.device_name

fdce8ab0f5796b0f0c177ddeb40896a48cb688cf
Arnaud de Coudenhove <arnaud.decoudenhove8@gmail.com>
2018-11-27 16:19:42 +0100

    integrate json files in subdevice class

--- raw_extractions/models.py
+++ raw_extractions/models.py
@@ -12,6 +12,8 @@ class EnergyName(models.Model):
     name = models.CharField(verbose_name="Energy Name", unique=True, max_length=50)
     category = models.ForeignKey('ControllerType', verbose_name="Category", on_delete='PROTECT')
 
+
+
     def __str__(self):
         return self.name
 
@@ -21,14 +23,18 @@ class SubDevice(models.Model):
     device_name = models.ForeignKey('Device', verbose_name='Device Name', on_delete='PROTECT')
     energy_names = models.ManyToManyField('EnergyName', verbose_name='Energy Name')
     category = models.ForeignKey('ControllerType', on_delete='PROTECT', verbose_name='Category')
+    device_value_history = models.FilePathField(verbose_name="Value_storage_file")
 
     def __str__(self):
         return self.device_name + self.category
 
 
+
 class Device(models.Model):
     # device_id = models.IntegerField(default=0, unique=True, verbose_name="device_id")
-    device_name = models.CharField(max_length=12, unique=True, verbose_name="device_name")
+    device_str = models.CharField(max_length=12, unique = True, verbose_name="device_str")
+    device_name = models.CharField(max_length=100, unique=True, verbose_name="device_name")
+    device_group = models.CharField(max_length=50, verbose_name="device_group")
 
     # energy_names = models.ManyToManyField('EnergyName', verbose_name='Energy Name')
     # category = models.ForeignKey('ControllerType', verbose_name="Category", on_delete='PROTECT')
